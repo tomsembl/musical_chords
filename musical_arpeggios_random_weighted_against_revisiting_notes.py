81758ab71f949baf5ef3985b2ae809220c94d85c
tomsembl <94670548+tomsembl@users.noreply.github.com>
2022-06-25 22:22:11 -0700

    Add files via upload

--- musical_arpeggios_random_weighted_against_revisiting_notes.py
+++ musical_arpeggios_random_weighted_against_revisiting_notes.py
@@ -38,45 +38,72 @@ INTERVALS = {
     "octave": 12,
 }
 SONGS = {
-    "howls_moving_castle": [ # [offset, chord]
-        (-INTERVALS["perfect_5th"],"minor_7th"),
-        (-INTERVALS["perfect_4th"],"major_♭7th"),
-        (0,"minor"),
-        (0,"minor_7th"),
-        (-INTERVALS["major_3rd"],"major_7th"),
-        (-INTERVALS["major_2nd"],"major_add2"),
-        (INTERVALS["minor_3rd"],"major"), 
-        (INTERVALS["minor_3rd"],"major_7th"),
-        (-INTERVALS["minor_3rd"],"diminished_7th"),
-        (INTERVALS["major_2nd"],"major_♭7th"),
-        (-INTERVALS["perfect_4th"],"minor_add4"),
-        (-INTERVALS["perfect_5th"],"major_6th"),
-        (-INTERVALS["major_6th"],"major_7th"),
-        (-INTERVALS["flat_7th"],"minor_7th"),
-        (-INTERVALS["perfect_4th"],"major"), 
-        (-INTERVALS["perfect_4th"],"major"),
+    "howls_moving_castle": [ # [offset, chord, beats]
+        (-INTERVALS["perfect_5th"],"minor_7th",3),
+        (-INTERVALS["perfect_4th"],"major_♭7th",3),
+        (0,"minor",3),
+        (0,"minor_7th",3),
+        (-INTERVALS["major_3rd"],"major_7th",3),
+        (-INTERVALS["major_2nd"],"major_add2",3),
+        (INTERVALS["minor_3rd"],"major",3), 
+        (INTERVALS["minor_3rd"],"major_7th",3),
+        (-INTERVALS["minor_3rd"],"diminished_7th",3),
+        (INTERVALS["major_2nd"],"major_♭7th",3),
+        (-INTERVALS["perfect_4th"],"minor_add4",3),
+        (-INTERVALS["perfect_5th"],"major_6th",3),
+        (-INTERVALS["major_6th"],"major_7th",3),
+        (-INTERVALS["flat_7th"],"minor_7th",3),
+        (-INTERVALS["perfect_4th"],"major",6),
+    ],
+    "fly_me_to_the_moon": [ # [offset, chord, beats]
+        (0,"minor",4),
+        (-INTERVALS["perfect_5th"],"minor_7th",4),
+        (-INTERVALS["major_2nd"],"major",2),
+        (-INTERVALS["major_2nd"],"major_♭7th",2),
+        (-INTERVALS["major_6th"],"major_7th",2),
+        (-INTERVALS["major_6th"],"major_♭7th",2),
+        (-INTERVALS["major_3rd"],"major",2),
+        (-INTERVALS["major_3rd"],"major_7th",2),
+        (-INTERVALS["perfect_5th"],"minor_7th",2),
+        (-INTERVALS["flat_7th"],"diminished_7th",2),
+        (-INTERVALS["perfect_4th"],"major_♭7th",4),
+        (0,"minor",2),
+        (0,"major_♭7th",2),
+        (-INTERVALS["perfect_5th"],"minor",2),
+        (-INTERVALS["perfect_5th"],"minor_7th",2),
+        (-INTERVALS["major_2nd"],"major",2),
+        (-INTERVALS["major_2nd"],"major_♭7th",2),
+        (-INTERVALS["major_6th"],"major_7th",4),
+        (0,"major_♭7th",4),
+        (-INTERVALS["perfect_5th"],"minor_7th",4),
+        (-INTERVALS["major_2nd"],"major_sus_4",2),
+        (-INTERVALS["major_2nd"],"major",2),
+        (-INTERVALS["major_6th"],"major_7th",4),
+        (-INTERVALS["perfect_4th"],"major",2),
+        (-INTERVALS["perfect_4th"],"major_♭7th",2),
+        
     ],
     "simple_twist_of_fate": [ # [offset, chord]
-        (0,"major"),
-        (0,"major_7th"),
-        (0,"major_♭7th"),
-        (-INTERVALS["perfect_5th"],"major"),
-        (-INTERVALS["perfect_5th"],"minor"),
-        (0,"major"), 
-        (-INTERVALS["perfect_4th"],"major_sus_4"),
-        (0,"major"),
+        (0,"major",4),
+        (0,"major_7th",4),
+        (0,"major_♭7th",4),
+        (-INTERVALS["perfect_5th"],"major",4),
+        (-INTERVALS["perfect_5th"],"minor",4),
+        (0,"major",4), 
+        (-INTERVALS["perfect_4th"],"major_sus_4",4),
+        (0,"major",4),
     ],
     "pop 4 chords minor": [
-        (0,"minor"),
-        (-INTERVALS["major_3rd"],"major"),
-        (INTERVALS["minor_3rd"],"major"),
-        (-INTERVALS["major_2nd"],"major"),
+        (0,"minor",4),
+        (-INTERVALS["major_3rd"],"major",4),
+        (INTERVALS["minor_3rd"],"major",4),
+        (-INTERVALS["major_2nd"],"major",4),
     ],
     "pop 4 chords major": [
-        (0,"major"),
-        (-INTERVALS["perfect_4th"],"major"),
-        (-INTERVALS["minor_3rd"],"minor"),
-        (-INTERVALS["perfect_5th"],"major"),
+        (0,"major",4),
+        (-INTERVALS["perfect_4th"],"major",4),
+        (-INTERVALS["minor_3rd"],"minor",4),
+        (-INTERVALS["perfect_5th"],"major",4),
     ],
 }
 
@@ -86,7 +113,7 @@ pygame.midi.init()
 player = pygame.midi.Output(0)
 instrument = 1
 player.set_instrument(instrument)
-STARTING_NOTE = 56
+STARTING_NOTE = 55
 while(True):
     for song_name in SONGS:
         song = SONGS[song_name]
@@ -99,20 +126,21 @@ while(True):
                 offset = chord[0]
                 root = STARTING_NOTE+offset
                 scale = CHORDS[chord[1]]
+                beats = chord[2]
                 print(MIDI_NOTE_NAMES[root%12], chord[1])
                 notes = [root+sum(scale[:i]) for i in range(len(scale)+1)]
                 notes += [root+12+sum(scale[:i+1]) for i in range(len(scale))]
                 player.note_on(notes[0]-12, 127)
                 if not last: last = 6
                 visited = []
-                for _ in range(12):
-                    weights = [0 if i == last else 0 if i in visited else 10**(2/abs((last-i))) for i,_ in enumerate(notes)]
+                for _ in range(beats*4):
+                    weights = [0 if i == last else 0 if i in visited else int(10**(2/abs((last-i)))) for i,_ in enumerate(notes)]
                     weights = [w-min(weights) for w in weights]
                     if set(weights) == {0}:
                         visited = visited[-1:]
-                        weights = [0 if i == last else 0 if i in visited else 10**(2/abs((last-i))) for i,_ in enumerate(notes)]
+                        weights = [0 if i == last else 0 if i in visited else int(10**(2/abs((last-i)))) for i,_ in enumerate(notes)]
                         weights = [w-min(weights) for w in weights]
-                    print(notes, weights)
+                    print("arpeggio:",notes,"probability:", weights)
                     note = random.choices(notes, weights=weights)[0]
                     last = notes.index(note)
                     visited.append(last)
